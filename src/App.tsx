ac25fa0ca4fdf67e9348439b3ade57ab8646e39e
Carlos Eduardo Teixeira <CTeixei4@its.jnj.com>
2023-06-13 10:47:26 -0300

    11 Custom Hooks Simple Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import "./App.css";
-import { Movie } from "./Model/Movie";
-import { MovieList } from "./components/MovieList/MovieList";
+import BackwardCounter from "./components/Counters/BackwardsCounter";
+import { ForwardCounter } from "./components/Counters/ForwardCounter";
 
 const App: React.FC = () => {
-    const dummyMovies: Movie[] = [
-        {
-            id: 1,
-            title: "Some Dummy Movie",
-            openingText: "This is the opening text of the movie",
-            releaseDate: "2021-05-18",
-        },
-        {
-            id: 2,
-            title: "Some Dummy Movie 2",
-            openingText: "This is the second opening text of the movie",
-            releaseDate: "2021-05-19",
-        },
-    ];
-
     return (
         <>
-            <section>
-                <button>Fetch Movies</button>
-            </section>
-            <section>
-                <MovieList movies={dummyMovies} />
-            </section>
+            <ForwardCounter />
+            <BackwardCounter />
         </>
     );
 };
---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,7 @@
+import styles from "./Card.module.css";
+
+const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return <div className={styles.card}>{children}</div>;
+};
+
+export { Card };
